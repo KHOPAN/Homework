98e47b3e423fddfa05dda726fe1f51268ec3d9a3
KHOPAN <puthipnakchan@gmail.com>
2025-02-08 15:45:32 +0700

    Initial commit

---
+++ sample-app/src/main/java/dev/oneuiproject/oneuiexample/base/FragmentInfo.java
@@ -0,0 +1,11 @@
+package dev.oneuiproject.oneuiexample.base;
+
+public interface FragmentInfo {
+    int getLayoutResId();
+
+    int getIconResId();
+
+    CharSequence getTitle();
+
+    boolean isAppBarEnabled();
+}
