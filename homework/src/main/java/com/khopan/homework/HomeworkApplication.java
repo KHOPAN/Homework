00dea6b7380882ed82f1cdad00e86b3200a82351
KHOPAN <puthipnakchan@gmail.com>
2025-12-28 23:07:47 +0700

    Add menus

--- homework/src/main/java/com/khopan/homework/HomeworkApplication.java
+++ homework/src/main/java/com/khopan/homework/HomeworkApplication.java
@@ -1,8 +1,13 @@
 package com.khopan.homework;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
 
 import com.khopan.core.activity.NavigationDrawerActivity;
 
@@ -10,5 +15,18 @@ public class HomeworkApplication extends NavigationDrawerActivity {
 	@Override
 	public void onCreate(@Nullable final Bundle bundle) {
 		super.onCreate(bundle);
+		this.addMenuProvider(new Provider());
+	}
+
+	private class Provider implements MenuProvider {
+		@Override
+		public void onCreateMenu(@NonNull final Menu menu, @NonNull final MenuInflater inflater) {
+			inflater.inflate(R.menu.menu, menu);
+		}
+
+		@Override
+		public boolean onMenuItemSelected(@NonNull final MenuItem item) {
+			return false;
+		}
 	}
 }
