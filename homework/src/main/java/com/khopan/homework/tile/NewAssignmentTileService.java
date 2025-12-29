eb9d528ce8724df0cf9522dc9fa4ae8d38b3f4f6
KHOPAN <puthipnakchan@gmail.com>
2025-12-29 14:30:18 +0700

    Add new assignment tile service

---
+++ homework/src/main/java/com/khopan/homework/tile/NewAssignmentTileService.java
@@ -0,0 +1,21 @@
+package com.khopan.homework.tile;
+
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+import android.service.quicksettings.TileService;
+
+import com.khopan.homework.activity.NewAssignmentActivity;
+
+public class NewAssignmentTileService extends TileService {
+	@Override
+	public void onClick() {
+		final Intent intent = new Intent(this, NewAssignmentActivity.class);
+
+		if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+			this.startActivityAndCollapse(PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE));
+		} else {
+			this.startActivityAndCollapse(intent);
+		}
+	}
+}
