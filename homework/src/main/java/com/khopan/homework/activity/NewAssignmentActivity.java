8f0473c646419b4dcc440294cd31ea14fe4c1d41
KHOPAN <puthipnakchan@gmail.com>
2025-12-29 16:07:15 +0700

    Add deadline settings

--- homework/src/main/java/com/khopan/homework/activity/NewAssignmentActivity.java
+++ homework/src/main/java/com/khopan/homework/activity/NewAssignmentActivity.java
@@ -1,25 +1,38 @@
 package com.khopan.homework.activity;
 
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
+import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatSpinner;
 import androidx.core.widget.NestedScrollView;
+import androidx.picker.app.SeslDatePickerDialog;
 
 import com.khopan.core.activity.ToolbarActivity;
 import com.khopan.homework.R;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
 import dev.oneuiproject.oneui.layout.ToolbarLayout;
+import dev.oneuiproject.oneui.widget.CardItemView;
 import dev.oneuiproject.oneui.widget.RoundedLinearLayout;
+import dev.oneuiproject.oneui.widget.Separator;
 
 public class NewAssignmentActivity extends ToolbarActivity {
+	private CardItemView deadlineView;
+	private LocalDate deadline;
+
 	@Override
 	public void onCreate(@Nullable final Bundle bundle) {
 		super.onCreate(bundle);
-		//this.toolbarLayout.setExpandable(false);
 		this.toolbarLayout.setShowNavigationButtonAsBack(true);
 		this.toolbarLayout.setTitle(this.getString(R.string.newAssignment));
 		final NestedScrollView scrollView = new NestedScrollView(this);
@@ -28,23 +41,48 @@ public class NewAssignmentActivity extends ToolbarActivity {
 		scrollView.setOverScrollMode(View.OVER_SCROLL_ALWAYS);
 		final LinearLayout linearLayout = new LinearLayout(this);
 		linearLayout.setOrientation(LinearLayout.VERTICAL);
+		final Separator subjectTitle = new Separator(this);
+		subjectTitle.setText(this.getString(R.string.subject));
+		linearLayout.addView(subjectTitle);
+		final RoundedLinearLayout subjectLayout = new RoundedLinearLayout(this);
+		subjectLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		subjectLayout.setBackgroundColor(this.getColor(dev.oneuiproject.oneui.design.R.color.oui_des_background_color));
+		subjectLayout.setOrientation(LinearLayout.VERTICAL);
+		final DisplayMetrics metrics = this.getResources().getDisplayMetrics();
+		final int subjectPaddingHorizontal = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 16.0f, metrics));
+		final int subjectPaddingVertical = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 8.0f, metrics));
+		subjectLayout.setPadding(subjectPaddingHorizontal, subjectPaddingVertical, subjectPaddingHorizontal, subjectPaddingVertical);
+		final AppCompatSpinner subjectSpinner = new AppCompatSpinner(this);
+		subjectSpinner.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		final ArrayAdapter<?> subjectAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, new String[] {"Math", "Physics", "Biology", "Add new subject"});
+		subjectAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		subjectSpinner.setAdapter(subjectAdapter);
+		subjectLayout.addView(subjectSpinner);
+		linearLayout.addView(subjectLayout);
+		linearLayout.addView(new Separator(this));
+		final RoundedLinearLayout deadlineLayout = new RoundedLinearLayout(this);
+		deadlineLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		deadlineLayout.setBackgroundColor(this.getColor(dev.oneuiproject.oneui.design.R.color.oui_des_background_color));
+		deadlineLayout.setOrientation(LinearLayout.VERTICAL);
+		this.deadlineView = new CardItemView(this);
+		this.deadlineView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+		this.deadlineView.setTitle(this.getString(R.string.deadline));
+		this.deadlineView.setClickable(true);
+		this.deadline = LocalDate.now();
+		this.deadlineView.setOnClickListener(view -> new SeslDatePickerDialog(this, (picker, year, month, day) -> {
+			this.deadline = LocalDate.of(year, month + 1, day);
+			this.updateDeadlineView();
+		}, this.deadline.getYear(), this.deadline.getMonthValue() - 1, this.deadline.getDayOfMonth()).show());
 
-		final RoundedLinearLayout serverLayout = new RoundedLinearLayout(this);
-		serverLayout.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-		serverLayout.setBackgroundColor(this.getColor(dev.oneuiproject.oneui.design.R.color.oui_des_background_color));
-		serverLayout.setOrientation(LinearLayout.VERTICAL);
-		/*final CardItemView serverAddressView = new CardItemView(this);
-		serverAddressView.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-		serverAddressView.setTitle("Server Address");
-		serverAddressView.setSummary("server.com");
-		serverAddressView.setClickable(true);
-		serverLayout.addView(serverAddressView);*/
-		final EditText edit = new EditText(this);
-		edit.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-		serverLayout.addView(edit);
-		linearLayout.addView(serverLayout);
+		this.updateDeadlineView();
+		deadlineLayout.addView(this.deadlineView);
+		linearLayout.addView(deadlineLayout);
 
 		scrollView.addView(linearLayout);
 		this.toolbarLayout.addView(scrollView);
+	}
+
+	private void updateDeadlineView() {
+		this.deadlineView.setSummary(this.deadline.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.getDefault())));
 	}
 }
