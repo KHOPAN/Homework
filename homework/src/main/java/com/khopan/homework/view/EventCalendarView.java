4a4c271b6ac2fd7628b7ca3dafa395eee4a70978
KHOPAN <puthipnakchan@gmail.com>
2026-01-19 07:15:15 +0700

    Test NestedScrollingParent3 logging

--- homework/src/main/java/com/khopan/homework/view/EventCalendarView.java
+++ homework/src/main/java/com/khopan/homework/view/EventCalendarView.java
@@ -3,13 +3,14 @@ package com.khopan.homework.view;
 import android.content.Context;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.core.view.NestedScrollingChild3;
+import androidx.core.view.NestedScrollingChildHelper;
 import androidx.core.view.NestedScrollingParent3;
-
-import com.google.android.material.appbar.CollapsingToolbarLayout;
+import androidx.core.view.NestedScrollingParentHelper;
 
 import org.jspecify.annotations.NonNull;
 import org.jspecify.annotations.Nullable;
@@ -17,6 +18,8 @@ import org.jspecify.annotations.Nullable;
 public class EventCalendarView extends ViewGroup implements NestedScrollingParent3, NestedScrollingChild3 {
 	private final View calendarView;
 	private final View eventView;
+	private final NestedScrollingChildHelper childHelper;
+	private final NestedScrollingParentHelper parentHelper;
 
 	private int divider = 500;
 
@@ -36,6 +39,79 @@ public class EventCalendarView extends ViewGroup implements NestedScrollingParen
 		this.eventView = new View(context);
 		this.eventView.setBackgroundColor(Color.BLUE);
 		this.addView(this.eventView);
+		this.childHelper = new NestedScrollingChildHelper(this);
+		this.parentHelper = new NestedScrollingParentHelper(this);
+		this.setNestedScrollingEnabled(true);
+	}
+
+	@Override
+	public boolean dispatchNestedPreScroll(final int dx, final int dy, final int @Nullable [] consumed, final int @Nullable [] offsetInWindow, final int type) {
+		final boolean result = this.childHelper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow, type);
+		Log.d("EventCalendarView", String.format("dispatchNestedPreScroll(%d, %d, {}, {}, %d) -> %s", dx, dy, type, result));
+		return result;
+	}
+
+	@Override
+	public boolean dispatchNestedScroll(final int dxConsumed, final int dyConsumed, final int dxUnconsumed, final int dyUnconsumed, final int @Nullable [] offsetInWindow, final int type) {
+		final boolean result = this.childHelper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow, type);
+		Log.d("EventCalendarView", String.format("dispatchNestedScroll(%d, %d, %d, %d, {}, %d) -> %s", dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type, result));
+		return result;
+	}
+
+	@Override
+	public void dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int @Nullable [] offsetInWindow, int type, int @NonNull [] consumed) {
+		Log.d("EventCalendarView", String.format("dispatchNestedScroll(%d, %d, %d, %d, {}, %d, {})", dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, type));
+		this.childHelper.dispatchNestedScroll(dxConsumed, dyConsumed, dxUnconsumed, dyUnconsumed, offsetInWindow, type, consumed);
+	}
+
+	@Override
+	public boolean hasNestedScrollingParent(final int type) {
+		final boolean result = this.childHelper.hasNestedScrollingParent(type);
+		Log.d("EventCalendarView", String.format("hasNestedScrollingParent(%d) -> %s", type, result));
+		return result;
+	}
+
+	@Override
+	public void onNestedPreScroll(@NonNull final View target, final int dx, final int dy, final int @NonNull [] consumed, final int type) {
+
+	}
+
+	@Override
+	public void onNestedScroll(@NonNull final View target, final int dxConsumed, final int dyConsumed, final int dxUnconsumed, final int dyUnconsumed, final int type, final int @NonNull [] consumed) {
+		consumed[1] = 300;
+	}
+
+	@Override
+	public void onNestedScroll(@NonNull final View target, final int dxConsumed, final int dyConsumed, final int dxUnconsumed, final int dyUnconsumed, final int type) {
+
+	}
+
+	@Override
+	public void onNestedScrollAccepted(@NonNull final View child, @NonNull final View target, final int axes, final int type) {
+
+	}
+
+	@Override
+	public boolean onStartNestedScroll(@NonNull final View child, @NonNull final View target, final int axes, final int type) {
+		return (axes & View.SCROLL_AXIS_VERTICAL) != 0;
+	}
+
+	@Override
+	public void onStopNestedScroll(@NonNull final View target, final int type) {
+
+	}
+
+	@Override
+	public boolean startNestedScroll(final int axes, final int type) {
+		final boolean result = this.childHelper.startNestedScroll(axes, type);
+		Log.d("EventCalendarView", String.format("startNestedScroll(%d, %d) -> %s", axes, type, result));
+		return result;
+	}
+
+	@Override
+	public void stopNestedScroll(final int type) {
+		Log.d("EventCalendarView", String.format("stopNestedScroll(%d)", type));
+		this.childHelper.stopNestedScroll(type);
 	}
 
 	@Override
@@ -52,68 +128,5 @@ public class EventCalendarView extends ViewGroup implements NestedScrollingParen
 		final int measuredWidth = MeasureSpec.makeMeasureSpec(width, MeasureSpec.EXACTLY);
 		this.calendarView.measure(measuredWidth, MeasureSpec.makeMeasureSpec(this.divider, MeasureSpec.EXACTLY));
 		this.eventView.measure(measuredWidth, MeasureSpec.makeMeasureSpec(height - this.divider, MeasureSpec.EXACTLY));
-	}
-
-	@Override
-	public void onNestedScroll(@NonNull final View target, final int dxConsumed, final int dyConsumed, final int dxUnconsumed, final int dyUnconsumed, final int type, final int @NonNull [] consumed) {
-		consumed[1] = 300;
-	}
-
-	@Override
-	public boolean onStartNestedScroll(@NonNull final View child, @NonNull final View target, final int axes, final int type) {
-		return (axes & View.SCROLL_AXIS_VERTICAL) != 0;
-	}
-
-	@Override
-	public void onNestedScrollAccepted(@NonNull final View child, @NonNull final View target, final int axes, final int type) {
-
-	}
-
-	@Override
-	public void onStopNestedScroll(@NonNull final View target, final int type) {
-
-	}
-
-	@Override
-	public void onNestedScroll(@NonNull final View target, final int dxConsumed, final int dyConsumed, final int dxUnconsumed, final int dyUnconsumed, final int type) {
-
-	}
-
-	@Override
-	public void onNestedPreScroll(@NonNull final View target, final int dx, final int dy, final int @NonNull [] consumed, final int type) {
-
-	}
-
-	@Override
-	public void dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int @Nullable [] offsetInWindow, int type, int @NonNull [] consumed) {
-
-	}
-
-	@Override
-	public boolean startNestedScroll(int axes, int type) {
-		return (axes & View.SCROLL_AXIS_VERTICAL) != 0;
-	}
-
-	@Override
-	public void stopNestedScroll(int type) {
-
-	}
-
-	@Override
-	public boolean hasNestedScrollingParent(int type) {
-		return false;
-	}
-
-	@Override
-	public boolean dispatchNestedScroll(int dxConsumed, int dyConsumed, int dxUnconsumed, int dyUnconsumed, int @Nullable [] offsetInWindow, int type) {
-		this.divider += dyUnconsumed;
-		this.requestLayout();
-		CollapsingToolbarLayout
-		return true;
-	}
-
-	@Override
-	public boolean dispatchNestedPreScroll(int dx, int dy, int @Nullable [] consumed, int @Nullable [] offsetInWindow, int type) {
-		return false;
 	}
 }
