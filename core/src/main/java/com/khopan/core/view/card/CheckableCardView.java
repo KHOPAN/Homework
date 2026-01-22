cfb54c675cc33369f85b3b84eedc5608ac3849fb
KHOPAN <puthipnakchan@gmail.com>
2026-01-22 09:45:08 +0700

    Add clickable/switch card view boilerplate

--- core/src/main/java/com/khopan/core/view/card/CheckableCardView.java
+++ core/src/main/java/com/khopan/core/view/card/CheckableCardView.java
@@ -1,4 +1,21 @@
 package com.khopan.core.view.card;
 
-public class CheckableCardView {
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CheckableCardView extends CardView {
+	public CheckableCardView(@NonNull final Context context) {
+		this(context, null, 0);
+	}
+
+	public CheckableCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet) {
+		this(context, attributeSet, 0);
+	}
+
+	public CheckableCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet, final int defaultStyleAttribute) {
+		super(context, attributeSet, defaultStyleAttribute);
+	}
 }
--- core/src/main/java/com/khopan/core/view/card/SwitchCardView.java
+++ core/src/main/java/com/khopan/core/view/card/SwitchCardView.java
@@ -1,4 +1,21 @@
 package com.khopan.core.view.card;
 
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
 public class SwitchCardView extends CheckableCardView {
+	public SwitchCardView(@NonNull final Context context) {
+		this(context, null, 0);
+	}
+
+	public SwitchCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet) {
+		this(context, attributeSet, 0);
+	}
+
+	public SwitchCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet, final int defaultStyleAttribute) {
+		super(context, attributeSet, defaultStyleAttribute);
+	}
 }
