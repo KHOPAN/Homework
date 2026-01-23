d19f74bc44d8bd538c7bef280b69d3863f241714
KHOPAN <puthipnakchan@gmail.com>
2026-01-23 23:03:37 +0700

    Add a checkbox to CheckableCardView

--- core/src/main/java/com/khopan/core/view/card/CheckableCardView.java
+++ core/src/main/java/com/khopan/core/view/card/CheckableCardView.java
@@ -2,11 +2,19 @@ package com.khopan.core.view.card;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatSeslCheckedTextView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import com.khopan.core.R;
 
 public class CheckableCardView extends CardView {
+	private final AppCompatSeslCheckedTextView checkbox;
+
 	public CheckableCardView(@NonNull final Context context) {
 		this(context, null, 0);
 	}
@@ -17,5 +25,22 @@ public class CheckableCardView extends CardView {
 
 	public CheckableCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet, final int defaultStyleAttribute) {
 		super(context, attributeSet, defaultStyleAttribute);
+		final ConstraintLayout constraintLayout = this.findViewById(R.id.constraint_layout);
+		this.checkbox = new AppCompatSeslCheckedTextView(context);
+		final TypedValue value = new TypedValue();
+		context.getTheme().resolveAttribute(android.R.attr.listChoiceIndicatorSingle, value, true);
+		this.checkbox.setCheckMarkDrawable(value.resourceId);
+		final ConstraintLayout.LayoutParams checkboxParams = new ConstraintLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+		final int constraintLayoutIdentifier = constraintLayout.getId();
+		checkboxParams.bottomToBottom = constraintLayoutIdentifier;
+		checkboxParams.startToStart = constraintLayoutIdentifier;
+		checkboxParams.topToTop = constraintLayoutIdentifier;
+		constraintLayout.addView(this.checkbox, checkboxParams);
+	}
+
+	@Override
+	public boolean performClick() {
+		this.checkbox.setChecked(!this.checkbox.isChecked());
+		return super.performClick();
 	}
 }
