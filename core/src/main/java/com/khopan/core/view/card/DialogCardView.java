71daa3aaaa0f85296e4805bf233a8e6599d2fb99
KHOPAN <puthipnakchan@gmail.com>
2026-01-25 00:18:16 +0700

    Add constructors to dialog card view

--- core/src/main/java/com/khopan/core/view/card/DialogCardView.java
+++ core/src/main/java/com/khopan/core/view/card/DialogCardView.java
@@ -1,4 +1,26 @@
 package com.khopan.core.view.card;
 
-public abstract class DialogCardView {
+import android.content.Context;
+import android.util.AttributeSet;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public abstract class DialogCardView extends SwitchableCardView {
+	public DialogCardView(@NonNull final Context context) {
+		this(context, null, 0);
+	}
+
+	public DialogCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet) {
+		this(context, attributeSet, 0);
+	}
+
+	public DialogCardView(@NonNull final Context context, @Nullable final AttributeSet attributeSet, final int defaultStyleAttribute) {
+		super(context, attributeSet, defaultStyleAttribute);
+		this.setOnClickListener(view -> this.showDialog());
+	}
+
+	protected void showDialog() {
+
+	}
 }
