13a54a89dda47cd6ec4d8a41885c8079d2d6a694
KHOPAN <puthipnakchan@gmail.com>
2026-02-04 19:07:55 +0700

    Layout the month button

--- homework/src/main/java/com/khopan/homework/view/CalendarPagerHolder.java
+++ homework/src/main/java/com/khopan/homework/view/CalendarPagerHolder.java
@@ -1,12 +1,17 @@
 package com.khopan.homework.view;
 
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.widget.ViewPager2;
 
-import com.khopan.core.view.SimpleViewHolder;
+import com.khopan.homework.R;
 
 public class CalendarPagerHolder {
 	final ViewPager2 viewPager;
@@ -20,23 +25,47 @@ public class CalendarPagerHolder {
 		this.viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
 	}
 
-	private class Adapter extends RecyclerView.Adapter<SimpleViewHolder<CalendarView>> {
+	private class Adapter extends RecyclerView.Adapter<ViewHolder> {
 		@Override
 		public int getItemCount() {
 			return 13;
 		}
 
 		@Override
-		public void onBindViewHolder(@NonNull final SimpleViewHolder<CalendarView> holder, final int position) {
+		public void onBindViewHolder(@NonNull final ViewHolder holder, final int position) {
 
 		}
 
 		@NonNull
 		@Override
-		public SimpleViewHolder<CalendarView> onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
-			final CalendarView calendarView = new CalendarView(CalendarPagerHolder.this.view, 6);
-			calendarView.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-			return new SimpleViewHolder<>(calendarView);
+		public ViewHolder onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+			final LinearLayout linearLayout = new LinearLayout(CalendarPagerHolder.this.view.context);
+			linearLayout.setLayoutParams(new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+			linearLayout.setOrientation(LinearLayout.VERTICAL);
+
+			final Button button = new Button(CalendarPagerHolder.this.view.context);
+			button.setBackgroundResource(dev.oneuiproject.oneui.design.R.drawable.oui_des_btn_transparent_bg);
+			button.setText("January");
+			button.setClickable(true);
+			button.setFocusable(true);
+			button.setTextSize(TypedValue.COMPLEX_UNIT_SP, 26.0f);
+			var x = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			x.gravity = Gravity.CENTER_HORIZONTAL;
+			x.bottomMargin = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 26.0f, CalendarPagerHolder.this.view.context.getResources().getDisplayMetrics()));
+			linearLayout.addView(button, x);
+			linearLayout.addView(new CalendarView(CalendarPagerHolder.this.view, 8), new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+
+			// 2.05625
+			return new ViewHolder(linearLayout);
+		}
+	}
+
+	private static class ViewHolder extends RecyclerView.ViewHolder {
+		private final LinearLayout linearLayout;
+
+		private ViewHolder(final LinearLayout linearLayout) {
+			super(linearLayout);
+			this.linearLayout = linearLayout;
 		}
 	}
 }
