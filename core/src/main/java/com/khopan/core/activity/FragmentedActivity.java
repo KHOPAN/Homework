8ee73aad59771031b48b58b96c849dd50ebd3eb9
KHOPAN <puthipnakchan@gmail.com>
2026-02-11 22:03:19 +0700

    Finish the FragmentedActivity

--- core/src/main/java/com/khopan/core/activity/FragmentedActivity.java
+++ core/src/main/java/com/khopan/core/activity/FragmentedActivity.java
@@ -12,18 +12,60 @@ import androidx.fragment.app.Fragment;
 import dev.oneuiproject.oneui.layout.ToolbarLayout;
 import dev.oneuiproject.oneui.widget.RoundedFrameLayout;
 
+/**
+ * An abstract {@link com.khopan.core.activity.ToolbarActivity}
+ * subclass that holds a {@link androidx.fragment.app.Fragment}.
+ */
 public abstract class FragmentedActivity extends ToolbarActivity {
+	/**
+	 * The {@link com.khopan.core.activity.FragmentedActivity.BackListener}.
+	 */
+	protected BackListener backListener;
+
+	/**
+	 * The {@link androidx.fragment.app.Fragment}.
+	 */
 	protected Fragment fragment;
-	protected RoundedFrameLayout fragmentLayout;
+
+	/**
+	 * The layout identifier for the internal
+	 * {@link dev.oneuiproject.oneui.widget.RoundedFrameLayout}.
+	 */
 	protected int frameLayoutIdentifier;
+
+	/**
+	 * The internal {@link dev.oneuiproject.oneui.widget.RoundedFrameLayout}.
+	 */
+	protected RoundedFrameLayout fragmentLayout;
+
+	/**
+	 * @return the {@link com.khopan.core.activity.FragmentedActivity.BackListener}.
+	 */
+	public BackListener getBackListener() {
+		return this.backListener;
+	}
 
 	@Override
 	public void onBackPressed() {
-		if(!(this.fragment instanceof BackListener && ((BackListener) this.fragment).backPressed())) {
+		if((!(this.fragment instanceof BackListener) || !((BackListener) this.fragment).backPressed()) && (this.backListener == null || !this.backListener.backPressed())) {
 			super.onBackPressed();
 		}
 	}
 
+	/**
+	 * Sets the {@link com.khopan.core.activity.FragmentedActivity.BackListener}.
+	 *
+	 * @param listener the {@link com.khopan.core.activity.FragmentedActivity.BackListener}.
+	 */
+	public void setBackListener(final BackListener listener) {
+		this.backListener = listener;
+	}
+
+	/**
+	 * Sets the {@link androidx.fragment.app.Fragment}.
+	 *
+	 * @param fragment the {@link androidx.fragment.app.Fragment}.
+	 */
 	public void setFragment(final Fragment fragment) {
 		if((this.fragment = fragment) == null) {
 			this.fragmentLayout.removeAllViews();
@@ -37,13 +79,21 @@ public abstract class FragmentedActivity extends ToolbarActivity {
 		super.onCreate(bundle);
 		this.fragmentLayout = new RoundedFrameLayout(this);
 		this.fragmentLayout.setId(this.frameLayoutIdentifier = View.generateViewId());
-		final int insets = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0f, this.getResources().getDisplayMetrics()));
+		final int insets = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0f, this.metrics));
 		this.fragmentLayout.setEdgeInsets(Insets.of(insets, 0, insets, 0));
 		this.toolbarLayout.addView(this.fragmentLayout, new ToolbarLayout.ToolbarLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 	}
 
+	/**
+	 * A listener for handling the back button.
+	 */
 	@FunctionalInterface
 	public interface BackListener {
+		/**
+		 * Handles the back button.
+		 *
+		 * @return true if the back button has been handled, false otherwise.
+		 */
 		boolean backPressed();
 	}
 }
