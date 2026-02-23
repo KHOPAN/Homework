3d392484495741e206f52388612a6d79b31ddfb0
KHOPAN <puthipnakchan@gmail.com>
2026-02-23 08:46:06 +0700

    Finish the calendar fragment

--- homework/src/main/java/com/khopan/homework/fragment/CalendarFragment.java
+++ homework/src/main/java/com/khopan/homework/fragment/CalendarFragment.java
@@ -1,6 +1,5 @@
 package com.khopan.homework.fragment;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
@@ -9,17 +8,16 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
+import com.khopan.core.fragment.ContextedFragment;
 import com.khopan.homework.calendar.EventCalendarView;
 
-public class CalendarFragment extends Fragment {
+public class CalendarFragment extends ContextedFragment {
 	@Nullable
 	@Override
 	public View onCreateView(@NonNull final LayoutInflater inflater, @Nullable final ViewGroup container, @Nullable final Bundle bundle) {
-		final Context context = inflater.getContext();
-		final EventCalendarView view = new EventCalendarView(context);
-		final int padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0f, context.getResources().getDisplayMetrics()));
+		final EventCalendarView view = new EventCalendarView(this.context);
+		final int padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0f, this.metrics));
 		view.setPadding(padding, 0, padding, 0);
 		return view;
 	}
