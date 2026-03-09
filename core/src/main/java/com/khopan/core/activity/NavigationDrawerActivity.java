aa301b7d2d9e673ff81157cd2009a011b93bbe6c
KHOPAN <puthipnakchan@gmail.com>
2026-03-09 09:39:52 +0700

    Improve the navigation drawer animation

--- core/src/main/java/com/khopan/core/activity/NavigationDrawerActivity.java
+++ core/src/main/java/com/khopan/core/activity/NavigationDrawerActivity.java
@@ -1,9 +1,14 @@
 package com.khopan.core.activity;
 
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Rect;
 import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,6 +19,7 @@ import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.appcompat.graphics.drawable.SeslRecoilDrawable;
 import androidx.appcompat.widget.AppCompatImageView;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -22,10 +28,13 @@ import androidx.slidingpanelayout.widget.SlidingPaneLayout;
 
 import com.khopan.core.CoreLayout;
 import com.khopan.core.R;
+import com.khopan.core.view.SimpleViewHolder;
+import com.khopan.core.view.card.CardView;
 
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import dev.oneuiproject.oneui.layout.DrawerLayout;
 import dev.oneuiproject.oneui.layout.NavDrawerLayout;
@@ -35,15 +44,16 @@ import dev.oneuiproject.oneui.utils.TypefaceUtilsKt;
 public abstract class NavigationDrawerActivity extends FragmentedActivity {
 	protected final List<DrawerEntry> drawerItems;
 
-	private Adapter adapter;
+	private final Adapter adapter;
 
 	protected NavigationDrawerActivity() {
 		this.drawerItems = new ArrayList<>();
+		this.adapter = new Adapter();
 	}
 
 	@Override
 	protected ToolbarLayout createToolbarLayout() {
-		return new NavDrawerLayout(this);
+		return (ToolbarLayout) this.getLayoutInflater().inflate(R.layout.layout_navigation_drawer, this.findViewById(android.R.id.content), false);
 	}
 
 	@Override
@@ -54,7 +64,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 		final ToolbarLayout.ToolbarLayoutParams recyclerViewParams = new ToolbarLayout.ToolbarLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
 		recyclerViewParams.layoutLocation = DrawerLayout.DRAWER_PANEL;
 		recyclerView.setLayoutParams(recyclerViewParams);
-		recyclerView.setAdapter(this.adapter = new Adapter());
+		recyclerView.setAdapter(this.adapter);
 		recyclerView.setHasFixedSize(true);
 		recyclerView.setItemAnimator(null);
 		recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -77,7 +87,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 					public void onPanelSlide(@NonNull final View view, final float time) {
 						final DisplayMetrics metrics = NavigationDrawerActivity.this.getResources().getDisplayMetrics();
 						final float iconSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 44.0f, metrics);
-						NavigationDrawerActivity.this.adapter.notifyItemRangeChanged(0, NavigationDrawerActivity.this.drawerItems.size(), new Payload(time, (int) (time * (((SlidingPaneLayout) containerLayout).seslGetPreferredDrawerPixelSize() - TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 85.0f / 3.0f, metrics) - iconSize) + iconSize)));
+						NavigationDrawerActivity.this.adapter.notifyItemRangeChanged(0, NavigationDrawerActivity.this.adapter.getItemCount(), new Payload(time, (int) (time * (((SlidingPaneLayout) containerLayout).seslGetPreferredDrawerPixelSize() - TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 85.0f / 3.0f, metrics) - iconSize) + iconSize)));
 					}
 				});
 			}
@@ -87,7 +97,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 	}
 
 	protected void setSelectedItem(final int position) {
-		this.adapter.selectItem(this.drawerItems.get(position), position);
+		//this.adapter.selectItem(this.drawerItems.get(position), position);
 	}
 
 	protected void onDrawerSelected(@NonNull final DrawerEntry entry, final boolean changed) {
@@ -118,7 +128,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 		}
 	}
 
-	private class Adapter extends RecyclerView.Adapter<ViewHolder> {
+	/*private class Adapter extends RecyclerView.Adapter<ViewHolder> {
 		private final Typeface normal;
 		private final Typeface selected;
 
@@ -160,10 +170,18 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 			holder.itemView.setSelected(selected);
 			holder.textView.setText(entry.text);
 			holder.textView.setTypeface(selected ? this.selected : this.normal);
-			final float time = ((NavDrawerLayout) NavigationDrawerActivity.this.toolbarLayout).getDrawerOffset();
-			holder.textView.setAlpha(time);
+
+			if(((NavDrawerLayout) NavigationDrawerActivity.this.toolbarLayout).isLargeScreenMode()) {
+				final float time = ((NavDrawerLayout) NavigationDrawerActivity.this.toolbarLayout).getDrawerOffset();
+				holder.textView.setAlpha(time);
+			}
+
 			final DisplayMetrics metrics = NavigationDrawerActivity.this.getResources().getDisplayMetrics();
 			final float iconSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 44.0f, metrics);
+
+			if(!((NavDrawerLayout) NavigationDrawerActivity.this.toolbarLayout).isLargeScreenMode()) {
+				return;
+			}
 
 			try {
 				final Method getContainerLayoutMethod = NavDrawerLayout.class.getDeclaredMethod("getContainerLayout$oneui_design_release");
@@ -172,6 +190,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 
 				if(containerLayout instanceof SlidingPaneLayout) {
 					final ViewGroup.LayoutParams params = holder.itemView.getLayoutParams();
+					final float time = ((NavDrawerLayout) NavigationDrawerActivity.this.toolbarLayout).getDrawerOffset();
 					params.width = (int) (time * (((SlidingPaneLayout) containerLayout).seslGetPreferredDrawerPixelSize() - TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 85.0f / 3.0f, metrics) - iconSize) + iconSize);
 					holder.itemView.setLayoutParams(params);
 				}
@@ -216,7 +235,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 			this.notifyItemChanged(this.selectedItem);
 			NavigationDrawerActivity.this.onDrawerSelected(entry, true);
 		}
-	}
+	}*/
 
 	private static class Payload {
 		private final float time;
@@ -228,7 +247,7 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 		}
 	}
 
-	private class ViewHolder extends RecyclerView.ViewHolder {
+	/*private class ViewHolder extends RecyclerView.ViewHolder {
 		private static final int VIEW_TYPE_SEPARATOR = 0;
 		private static final int VIEW_TYPE_DRAWER_ITEM = 1;
 
@@ -252,6 +271,148 @@ public abstract class NavigationDrawerActivity extends FragmentedActivity {
 			parameters.topMargin = parameters.bottomMargin = NavigationDrawerActivity.this.getPixelSize(6.0f);
 			this.itemView.setLayoutParams(parameters);
 			this.itemView.setForeground(AppCompatResources.getDrawable(NavigationDrawerActivity.this, R.drawable.drawer_separator));
+		}
+	}*/
+
+	private class Adapter extends RecyclerView.Adapter<SimpleViewHolder<CardView>> {
+		@Override
+		public int getItemCount() {
+			return 100;
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull final SimpleViewHolder<CardView> holder, final int position) {
+			holder.itemView.setTitle("Hello, world!");
+			//holder.itemView.setSummary("Hi");
+			holder.itemView.setTopDividerVisible(false);
+			holder.itemView.constraintLayout.setForeground(new ResizableDrawable(R.drawable.drawer_ripple));
+			holder.itemView.setBackground(new ResizableDrawable(R.drawable.drawer_selector));
+			holder.itemView.setIcon(AppCompatResources.getDrawable(NavigationDrawerActivity.this, NavigationDrawerActivity.this.drawerItems.get(0).icon));
+			holder.itemView.setOnClickListener(view -> {
+				holder.itemView.setSelected(!holder.itemView.isSelected());
+			});
+		}
+
+		@Override
+		public void onBindViewHolder(@NonNull final SimpleViewHolder<CardView> holder, final int position, @NonNull final List<Object> payloads) {
+			if(payloads.isEmpty()) {
+				this.onBindViewHolder(holder, position);
+				return;
+			}
+
+			for(final Object payload : payloads) {
+				if(!(payload instanceof Payload)) {
+					continue;
+				}
+
+				final Payload value = (Payload) payload;
+				holder.itemView.titleView.setAlpha(value.time);
+				((ResizableDrawable) holder.itemView.getBackground()).setWidth(value.width);
+				((ResizableDrawable) holder.itemView.constraintLayout.getForeground()).setWidth(value.width);
+			}
+		}
+
+		@NonNull
+		@Override
+		public SimpleViewHolder<CardView> onCreateViewHolder(@NonNull final ViewGroup parent, final int viewType) {
+			final CardView cardView = new CardView(parent.getContext());
+			final RecyclerView.LayoutParams cardViewParams = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			final int horizontal = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 14.0f, NavigationDrawerActivity.this.metrics));
+			final int vertical = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 6.0f, NavigationDrawerActivity.this.metrics));
+			cardViewParams.bottomMargin = vertical;
+			cardViewParams.leftMargin = horizontal;
+			cardViewParams.rightMargin = horizontal;
+			cardViewParams.topMargin = vertical;
+			cardView.setLayoutParams(cardViewParams);
+			return new SimpleViewHolder<>(cardView);
+		}
+	}
+
+	private class ResizableDrawable extends Drawable implements Drawable.Callback {
+		private final Drawable drawable;
+
+		private int width;
+
+		private ResizableDrawable(final int drawable) {
+			this.drawable = Objects.requireNonNull(AppCompatResources.getDrawable(NavigationDrawerActivity.this, drawable));
+			this.drawable.setCallback(this);
+			this.width = -1;
+		}
+
+		@Override
+		public void draw(@NonNull final Canvas canvas) {
+			this.drawable.draw(canvas);
+		}
+
+		@Override
+		public int getIntrinsicHeight() {
+			return this.drawable.getIntrinsicHeight();
+		}
+
+		@Override
+		public int getIntrinsicWidth() {
+			return this.width < 0 ? this.drawable.getIntrinsicWidth() : this.width;
+		}
+
+		@Override
+		public int getOpacity() {
+			return this.drawable.getOpacity();
+		}
+
+		@Override
+		public void invalidateDrawable(@NonNull final Drawable drawable) {
+			this.invalidateSelf();
+		}
+
+		@Override
+		public boolean isStateful() {
+			return this.drawable.isStateful();
+		}
+
+		@Override
+		public void scheduleDrawable(@NonNull final Drawable drawable, @NonNull final Runnable runnable, final long time) {
+			this.scheduleSelf(runnable, time);
+		}
+
+		@Override
+		public void setAlpha(final int alpha) {
+			this.drawable.setAlpha(alpha);
+		}
+
+		@Override
+		public void setColorFilter(@Nullable final ColorFilter colorFilter) {
+			this.drawable.setColorFilter(colorFilter);
+		}
+
+		@Override
+		public void unscheduleDrawable(@NonNull final Drawable drawable, @NonNull final Runnable runnable) {
+			this.unscheduleSelf(runnable);
+		}
+
+		@Override
+		protected void onBoundsChange(@NonNull final Rect bounds) {
+			this.drawable.setBounds(bounds.left, bounds.top, this.width < 0 ? bounds.right : (this.width - bounds.left), bounds.bottom);
+		}
+
+		@Override
+		protected boolean onLevelChange(final int level) {
+			return this.drawable.setLevel(level);
+		}
+
+		@Override
+		protected boolean onStateChange(@NonNull final int[] state) {
+			final boolean changed = this.drawable.setState(state);
+
+			if(changed) {
+				this.invalidateSelf();
+			}
+
+			return changed;
+		}
+
+		private void setWidth(final int width) {
+			this.width = width;
+			this.drawable.setBounds(0, 0, this.width, this.drawable.getBounds().bottom);
 		}
 	}
 }
