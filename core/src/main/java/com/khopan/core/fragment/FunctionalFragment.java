6280535815e916ca566bb09c4fe5cdbe1cfe0da3
KHOPAN <puthipnakchan@gmail.com>
2026-03-11 14:02:03 +0700

    Document FunctionalFragment

--- core/src/main/java/com/khopan/core/fragment/FunctionalFragment.java
+++ core/src/main/java/com/khopan/core/fragment/FunctionalFragment.java
@@ -5,11 +5,16 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.widget.NestedScrollView;
 
+/**
+ * A {@link com.khopan.core.fragment.ContextedFragment} with
+ * a built-in {@link androidx.core.widget.NestedScrollView}.
+ */
 public abstract class FunctionalFragment extends ContextedFragment {
 	@Nullable
 	@Override
@@ -22,11 +27,16 @@ public abstract class FunctionalFragment extends ContextedFragment {
 		final NestedScrollView scrollView = (NestedScrollView) view;
 		scrollView.setFillViewport(true);
 		scrollView.setOverScrollMode(View.OVER_SCROLL_ALWAYS);
-		final int padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0f, this.context.getResources().getDisplayMetrics()));
+		final int padding = Math.round(TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 10.0f, this.metrics));
 		scrollView.setPadding(padding, 0, padding, 0);
-		scrollView.addView(this.initialize());
+		scrollView.addView(this.initialize(), new FrameLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
 	}
 
+	/**
+	 * Initializes the {@link android.view.View}.
+	 *
+	 * @return the {@link android.view.View}.
+	 */
 	protected View initialize() {
 		return null;
 	}
